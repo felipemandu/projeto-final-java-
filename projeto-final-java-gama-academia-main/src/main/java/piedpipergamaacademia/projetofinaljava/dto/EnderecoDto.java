4cc3c53d0fe0442fa83c766b47dab06984bb9b1f
laismarques <lsmaarques@gmail.com>
2021-02-24 23:52:12 -0300

    Atualizando arquivos

---
+++ projeto-final-java-gama-academia-main/src/main/java/piedpipergamaacademia/projetofinaljava/dto/EnderecoDto.java
@@ -0,0 +1,76 @@
+package piedpipergamaacademia.projetofinaljava.dto;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
+
+import org.hibernate.validator.constraints.Length;
+
+public class EnderecoDto {
+
+	@NotEmpty
+	@Length(min=5)
+    private String rua;
+	
+	@NotEmpty
+    private String numero;
+	
+    private String complemento;
+
+	@NotEmpty
+	@Length(min=3)
+    private String cidade;
+
+    @Pattern(regexp = "[A-Z]{2}")
+    private String uf;
+
+    @Pattern(regexp = "[0-9]{5}-[0-9]{3}")
+    private String cep;
+
+    public String getRua() {
+        return rua;
+    }
+
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+
+    public String getUf() {
+        return uf;
+    }
+
+    public void setUf(String uf) {
+        this.uf = uf;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+}
